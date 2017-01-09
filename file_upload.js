25ea86056f6c699d165477091c768bdfcaeb7f95
good <bieshuohua@users.noreply.github.com>
2017-01-09 10:47:48 +0800

    Update and rename Dec/javascript.js to file_upload.js

--- file_upload.js
+++ file_upload.js
@@ -51,3 +51,40 @@ $("a[data-nodelete=False]").live({
         DeleteFile(this, '', delCallback, true)
     }
 });
+
+//文件存在验证
+function beforeSubmit() {
+    if (!$("form").eq(0).Validform().check()) {
+        return false;
+    } else {
+        var flag = true;
+        $.ajax({
+            url: "/PaperWorks/CheckFile",
+            dataType: "json",
+            type: "POST",
+            data: { guid: $("#GUID").val() },
+            async: false,
+            success: function (data) {
+                if (!data.isOk) {
+                    artDialog.alert("经费拨付文件未上传！");
+                    flag = false;
+                }
+            }
+        });
+        return flag;
+    }
+    showLoading();
+}
+//js调用的c#控制器方法，查询Attachment表外键为原GUID的项是否存在
+public JsonResult CheckFile(string guid)//c# code
+{
+    var criteria = new NameValueCollection();
+    criteria.Add(GetPropertyName<Model.Attachment>(m => m.FID), guid);
+    criteria.Add(CreateWhereCriteria<Model.Attachment>(m => m.IsDeleted, "1", Configuration.Enums.MeanOperator.NotEqual));
+    var isExists = new BLL.Attachment().Exists(criteria);
+    return Json(new { isExist = isExists });
+}
+//删除按钮回调函数，即时的在页面删除文件
+function afterDelete(curid) {
+    $("a[curid=" + curid + "]").parents("li").remove();
+}
